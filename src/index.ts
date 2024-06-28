cac9f1b7588582663d1b8ba0d2ccc715052e0319
Dominic Fournier <me@dominicfournier.com>
2024-06-28 15:51:38 -0400

    Added IP

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import app from './app';
+import app from "./app";
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
+const port = Number.parseInt(process.env.PORT || "5000");
+app.listen(port, "0.0.0.0", () => {
   /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);
   /* eslint-enable no-console */
