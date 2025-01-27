aed30fb079c1b6064731478786331800986cc2e1
unknown <me@dominicfournier.com>
2025-01-26 22:20:55 -0500

    Dont log env

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ app.listen(config.host.port, config.host.domain, () => {
   console.log(
     `Server is running on http://${config.host.domain}:${config.host.port}`
   );
-  // if (config.env.node_env === "development" && config.env.debug) {
-  console.log(`With provided configuration`, config);
-  // }
+  if (config.env.node_env === "development" && config.env.debug) {
+    console.log(`With provided configuration`, config);
+  }
 });
